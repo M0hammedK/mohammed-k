d2b055d2c35a2c0501e4a3f946eb6a02b1cb344a
M0hammedK <mmm2016alkaf@gmail.com>
2025-01-27 22:03:41 +0300

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    { pattern: /.*/ }, // Safelist all classes
+  ],
   theme: {
     extend: {
       fontFamily:{
